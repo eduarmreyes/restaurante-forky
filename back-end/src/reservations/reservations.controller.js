abdeddeeb872c4ab5c8a931b7008d0e016666812
Diego <32918727+diegoFMR@users.noreply.github.com>
2023-07-07 15:06:34 -0400

    console log response for reservations

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -2,6 +2,11 @@
  * List handler for reservation resources
  */
 async function list(req, res) {
+  try{
+    console.log('request received');
+  }catch(e){
+    console.log(e);
+  }
   res.json({
     data: [],
   });
