218fc457aa616ac00ef12bf8c799d8bf0514252d
eduarmreyes <eduar.mreyes@gmail.com>
2023-07-25 23:35:09 -0700

    use same form on reservation

--- front-end/src/reservations/Reservation.js
+++ front-end/src/reservations/Reservation.js
@@ -2,7 +2,8 @@ import React, { useEffect, useState } from "react";
 import { listTables, getReservation, sendUpdate } from "../utils/api";
 import ErrorAlert from "../layout/ErrorAlert";
 import TableSelect from "../table/TableSelect";
-import {useParams, useHistory} from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
+import ReservationForm from "./ReservationForm";
 /**
  * Defines the Reservation page.
  * @param date
@@ -10,24 +11,27 @@ import {useParams, useHistory} from "react-router-dom";
  * @returns {JSX.Element}
  */
 function Reservation() {
-  const [tables, setTables] = useState([]);//array of tables
+  const [tables, setTables] = useState([]); //array of tables
   const [requestError, setrequestError] = useState(null);
-  const [table, setTable] = useState('');//table selected
-  const [reservation, setReservation] = useState('');
-  const {reservation_id} = useParams();//Id from params
+  const [table, setTable] = useState(""); //table selected
+  const [reservation, setReservation] = useState("");
+  const { reservation_id } = useParams(); //Id from params
   const history = useHistory();
 
-  useEffect(()=>{
+  useEffect(() => {
     const abortController = new AbortController();
 
     async function loadTables() {
-      try{
-        const response = await getReservation(reservation_id, abortController.signal);
-        setReservation(response)
+      try {
+        const response = await getReservation(
+          reservation_id,
+          abortController.signal
+        );
+        setReservation(response);
         const tableResponse = await listTables(abortController.signal);
         setTables(tableResponse);
-      }catch(e){
-        setrequestError({error: "Couldn't load the records"});
+      } catch (e) {
+        setrequestError({ error: "Couldn't load the records" });
       }
     }
 
@@ -36,42 +40,51 @@ function Reservation() {
     return () => abortController.abort();
   }, [reservation_id]);
 
-  async function onSubmit(e){
+  async function onSubmit(e) {
     e.preventDefault();
-    try{
-      if(table !== ""){
+    try {
+      if (table !== "") {
         const abortController = new AbortController();
-        const data = {reservation_id: reservation.reservation_id, people: reservation.people};
-        const response = await sendUpdate({data: data}, `tables/${table}/seat`,abortController.signal);
-        
-        if(response.status === 400 || response.status === 404){
-            const msg = await response.json();
-            setrequestError({message: msg.error});
-        }else{
-            history.push('/dashboard');
+        const data = {
+          reservation_id: reservation.reservation_id,
+          people: reservation.people,
+        };
+        const response = await sendUpdate(
+          { data: data },
+          `tables/${table}/seat`,
+          abortController.signal
+        );
+
+        if (response.status === 400 || response.status === 404) {
+          const msg = await response.json();
+          setrequestError({ message: msg.error });
+        } else {
+          history.push("/dashboard");
         }
-      }else{
-        setrequestError({message: "Please select a table"});
+      } else {
+        setrequestError({ message: "Please select a table" });
       }
-        
-    }catch(e){
-        console.log(e);
+    } catch (e) {
+      console.log(e);
     }
   }
 
   return (
     <main>
-      <h1> Reservation: {reservation.first_name} - {reservation.last_name} </h1>
+      <h1>
+        {" "}
+        Reservation: {reservation.first_name} - {reservation.last_name}{" "}
+      </h1>
       <div className="d-md-flex mb-3">
         <p className="mb-0">People: {reservation.people}</p>
       </div>
       <h4 className="mb-0">Select a table</h4>
-    <form onSubmit={onSubmit}>
+      <ReservationForm />
+      {/* <form onSubmit={onSubmit}>
       <TableSelect items={tables} table={table} setTable={setTable} />
       <ErrorAlert error={requestError} />
       <button className="btn" type="submit">Assign to table</button>
-    </form>
-      
+    </form> */}
     </main>
   );
 }
--- front-end/src/reservations/ReservationForm.js
+++ front-end/src/reservations/ReservationForm.js
@@ -10,131 +10,147 @@ import ErrorAlert from "../layout/ErrorAlert";
  * @returns {JSX.Element}
  */
 function ReservationForm() {
-  const [name, setName] = useState('');
-  const [lastName, setLastName] = useState('');
-  const [mobileNumber, setMobileNumber] = useState('');
-  const [reserveDate, setReserveDate] = useState('');
-  const [reserveTime, setReserveTime] = useState('');
-  const [people, setPeople] = useState('');
+  const [name, setName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [mobileNumber, setMobileNumber] = useState("");
+  const [reserveDate, setReserveDate] = useState("");
+  const [reserveTime, setReserveTime] = useState("");
+  const [people, setPeople] = useState("");
   const [reservationsError, setReservationsError] = useState(null);
   const history = useHistory();
   const params = useParams();
 
-  useEffect(()=>{
+  useEffect(() => {
     const abortController = new AbortController();
 
-    async function retrieveReservation(){  
-      try{
-        const response = await getReservation( params.reservation_id, abortController.signal);
-        
+    async function retrieveReservation() {
+      try {
+        const response = await getReservation(
+          params.reservation_id,
+          abortController.signal
+        );
+
         setName(response.first_name);
         setLastName(response.last_name);
         setMobileNumber(response.mobile_number);
-        const date = new Date(response.reservation_date)
+        const date = new Date(response.reservation_date);
         setReserveDate(formatAsDate(date.toISOString()));
-  
+
         setReserveTime(response.reservation_time);
         setPeople(response.people);
-
-      }catch(e){
-        console.log(e)
+      } catch (e) {
+        console.log(e);
       }
-
-      
     }
-    
-    if(params.reservation_id){
+
+    if (params.reservation_id) {
       retrieveReservation();
     }
-    
-    return ()=>abortController.abort()
 
-  }, [params.reservation_id])
+    return () => abortController.abort();
+  }, [params.reservation_id]);
 
-  function cancelClick(e){
+  function cancelClick(e) {
     e.preventDefault();
     history.goBack();
   }
 
-  function dateValidation(){
+  function dateValidation() {
     const receivedDate = new Date(reserveDate);
     const todayDate = new Date();
-    const errors = [];//array containing the error messages
-    let valid = true;//value will be false if a validation fails
+    const errors = []; //array containing the error messages
+    let valid = true; //value will be false if a validation fails
 
-    if(receivedDate < todayDate){
+    if (receivedDate < todayDate) {
       errors.push("Only future dates are accepted");
       valid = false;
     }
 
-    if(receivedDate.getDay() === 1){
+    if (receivedDate.getDay() === 1) {
       errors.push("Cannot make reservation for Tuesdays");
       valid = false;
     }
 
-    if(!valid){
-      const message = errors.map(value=><p key={value}>{value}</p>);//concatenating error messages
-      setReservationsError({message: message})
+    if (!valid) {
+      const message = errors.map((value) => <p key={value}>{value}</p>); //concatenating error messages
+      setReservationsError({ message: message });
     }
 
     return valid;
-    
   }
-  function phoneValidation(){
+  function phoneValidation() {
     const regFull = new RegExp(/[0-9]{3}-[0-9]{3}-[0-9]{4}/);
     const regNumber = new RegExp(/[0-9]{3}[0-9]{3}[0-9]{4}/);
-    if(regFull.test(mobileNumber)){
+    if (regFull.test(mobileNumber)) {
       return true;
-    }else if(regNumber.test(mobileNumber)){
+    } else if (regNumber.test(mobileNumber)) {
       setMobileNumber(
-        mobileNumber[0]+mobileNumber[1]+mobileNumber[2]+"-"
-        +mobileNumber[3]+mobileNumber[4]+mobileNumber[5]+"-"
-        +mobileNumber[6]+mobileNumber[7]+mobileNumber[8]
-      )
-        return true;
-    }else{
-      setReservationsError({message: "The phone number does not meet format criteria"});
+        mobileNumber[0] +
+          mobileNumber[1] +
+          mobileNumber[2] +
+          "-" +
+          mobileNumber[3] +
+          mobileNumber[4] +
+          mobileNumber[5] +
+          "-" +
+          mobileNumber[6] +
+          mobileNumber[7] +
+          mobileNumber[8]
+      );
+      return true;
+    } else {
+      setReservationsError({
+        message: "The phone number does not meet format criteria",
+      });
       return false;
     }
   }
 
-  async function onSubmit(e){
+  async function onSubmit(e) {
     e.preventDefault();
 
-    try{
-
-      if(dateValidation() && phoneValidation()){
+    try {
+      if (dateValidation() && phoneValidation()) {
         const abortController = new AbortController();
         const reserveObj = {
-            "first_name": name,
-            "last_name": lastName,
-            "mobile_number":mobileNumber,
-            "reservation_date":reserveDate,
-            "reservation_time":reserveTime,
-            "people": parseInt(people)
-        }
+          first_name: name,
+          last_name: lastName,
+          mobile_number: mobileNumber,
+          reservation_date: reserveDate,
+          reservation_time: reserveTime,
+          people: parseInt(people),
+        };
         let response;
         let msg;
-        if(params.reservation_id){
+        if (params.reservation_id) {
           reserveObj.reservation_id = params.reservation_id;
 
-          response = await sendUpdate({data: reserveObj}, 'reservations/'+reserveObj.reservation_id, abortController.signal);
+          response = await sendUpdate(
+            { data: reserveObj },
+            "reservations/" + reserveObj.reservation_id,
+            abortController.signal
+          );
           msg = "Something went wrong. Please try again";
-        }else{
-          response = await insertReservation( {data: reserveObj} ,abortController.signal);
+        } else {
+          response = await insertReservation(
+            { data: reserveObj },
+            abortController.signal
+          );
           msg = "The time has to be after 10:30 AM and before 9:30 PM";
         }
 
-        if(!response){
-          setReservationsError({message: msg});
-        }else{
+        if (!response) {
+          setReservationsError({ message: msg });
+        } else {
           history.push(`/dashboard?date=${reserveDate}`);
         }
-        
       }
-    }catch(e){
-        setReservationsError({message: "Make sure the date is greater than today and all inputs are correct"});
-        console.log(e);
+    } catch (e) {
+      setReservationsError({
+        message:
+          "Make sure the date is greater than today and all inputs are correct",
+      });
+      console.log(e);
     }
   }
 
@@ -143,50 +159,67 @@ function ReservationForm() {
       <h1>Form</h1>
       <div className="d-md-flex mb-3">
         <form onSubmit={onSubmit}>
-            <label>First name</label>
-            <input name="first_name" 
-                required
-                onChange={(e)=>setName(e.target.value)}
-                value={name} />
-            <label>last name</label>
-            <input name="last_name" 
-                required
-                onChange={(e)=>setLastName(e.target.value)}
-                value={lastName} />
-             <label>Mobile Number</label>
-            <input name="mobile_number" 
-                type="tel"
-                required 
-                onChange={(e)=>setMobileNumber(e.target.value)}
-                value={mobileNumber} />
-            <label>Reservation Date</label>
-            <input name="reservation_date" 
-                type="date" 
-                placeholder="YYYY-MM-DD" 
-                pattern="\d{4}-\d{2}-\d{2}"
-                onChange={(e)=>setReserveDate(e.target.value)}
-                value={reserveDate} />
-            <label>Time</label>
-            <input name="reservation_time" 
-                type="time" 
-                placeholder="HH:MM" 
-                pattern="[0-9]{2}:[0-9]{2}"
-                onChange={(e)=>setReserveTime(e.target.value)}
-                value={reserveTime} />
-            <label>People</label>
-            <input name="people" 
-                required
-                onChange={(e)=>setPeople(e.target.value)}
-                value={people} /> 
+          <label>First name</label>
+          <input
+            name="first_name"
+            required
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+          />
+          <label>last name</label>
+          <input
+            name="last_name"
+            required
+            onChange={(e) => setLastName(e.target.value)}
+            value={lastName}
+          />
+          <label>Mobile Number</label>
+          <input
+            name="mobile_number"
+            type="tel"
+            required
+            onChange={(e) => setMobileNumber(e.target.value)}
+            value={mobileNumber}
+          />
+          <label>Reservation Date</label>
+          <input
+            name="reservation_date"
+            type="date"
+            placeholder="YYYY-MM-DD"
+            pattern="\d{4}-\d{2}-\d{2}"
+            onChange={(e) => setReserveDate(e.target.value)}
+            value={reserveDate}
+          />
+          <label>Time</label>
+          <input
+            name="reservation_time"
+            type="time"
+            placeholder="HH:MM"
+            pattern="[0-9]{2}:[0-9]{2}"
+            onChange={(e) => setReserveTime(e.target.value)}
+            value={reserveTime}
+          />
+          <label>People</label>
+          <input
+            name="people"
+            required
+            onChange={(e) => setPeople(e.target.value)}
+            value={people}
+          />
 
-            <ErrorAlert error={reservationsError}/>
-            
-            <button type="submit">Submit</button>
-            <button type="button"
-              onClick={(e)=>{cancelClick(e)}}>cancel</button>
+          <ErrorAlert error={reservationsError} />
+
+          <button type="submit">Submit</button>
+          <button
+            type="button"
+            onClick={(e) => {
+              cancelClick(e);
+            }}
+          >
+            cancel
+          </button>
         </form>
       </div>
-      
     </div>
   );
 }
